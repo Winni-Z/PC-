6e65c200f4f1983fbe0d1266f62fb08d6bf6b21d
winni <499207247@qq.com>
2019-01-04 20:04:20 +0800

    头部小箭头样式完成

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-/**
- * Created by Asus on 2019/1/4.
- */
+
+window.onload=function () {
+
+
+    // 获取元素
+    var arrowNode=document.querySelector('.arrow');
+    var headeraLisNode=document.querySelectorAll('.nav li');
+    var headerDownNodes=document.querySelectorAll('.down');
+
+    arrowNode.style.left=headeraLisNode[0].getBoundingClientRect().left+headeraLisNode[0].offsetWidth/2-arrowNode.offsetWidth/2+'px'
+    headerDownNodes[0].style.width='100%'
+    for (var i = 0; i < headeraLisNode.length; i++) {
+        headeraLisNode[i].index=i;
+        headeraLisNode[i].onclick=function () {
+            for (var j = 0; j < headerDownNodes.length; j++) {
+                 headerDownNodes[j].style.width='0';
+            }
+        headerDownNodes[this.index].style.width='100%';
+        arrowNode.style.left=this.getBoundingClientRect().left+this.offsetWidth/2-arrowNode.offsetWidth/2+'px'
+
+       }
+   }
+
+
+
+
+}
